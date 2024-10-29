9fc40c9057296a4ba94cef41fbcc7134719955d5
jherak2904 <jherakp@gmail.com>
2024-10-29 10:13:01 -0500

    asd

--- src/components/userHome.jsx
+++ src/components/userHome.jsx
@@ -16,7 +16,7 @@ export default function UserHome({ user }) {
 
       try {
         const response = await fetch(
-          "https://promocion-back.vercel.app/codes/getUserCodes",
+          "https://backparcial.vercel.app/codes/getUserCodes",
           {
         
             method: "POST",
@@ -46,7 +46,7 @@ export default function UserHome({ user }) {
     event.preventDefault();
     try {
       const response = await fetch(
-        "https://promocion-back.vercel.app/codes/registerCode",
+        "https://backparcial.vercel.app/codes/registerCode",
         {
       
           method: "POST",
